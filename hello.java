fad9fee0319d7b20e7757ffe3047c5c080627087
Tasnuba-Tabassum <tasnubatabassum593@gmail.com>
2023-12-22 20:38:52 +0600

    first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args){
+        int myNum;
+        myNum = 99;
+        System.out.println(myNum);
+    }
+}
---
+++ trying.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class trying {
+    public static void main(String[] args){
+        Scanner oishi = new Scanner(System.in);
+         int number;
+        System.out.print("Enter any number: ");
+        number = oishi.nextInt();
+        System.out.println("Number = " + number);
+    }
+}
