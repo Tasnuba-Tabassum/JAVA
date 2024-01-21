8b3c332f9f98dab85354d15368e2d18f6ac2eda6
Tasnuba-Tabassum <tasnubatabassum593@gmail.com>
2024-01-22 00:41:49 +0600

    A

---
+++ constructors.java
@@ -0,0 +1,13 @@
+public class constructors {
+    int x;  // Create a class attribute
+
+  // Create a class constructor for the Main class
+  public constructors() {
+    x = 5;  // Set the initial value for the class attribute x
+  }
+
+  public static void main(String[] args) {
+    constructors myObj = new constructors(); // Create an object of class Main (This will call the constructor)
+    System.out.println(myObj.x); // Print the value of x
+  }
+}
